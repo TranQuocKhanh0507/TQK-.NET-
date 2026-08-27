8d48fa5a2dc3d5bc6d46e4a8a593996ebe4af67d
TranQuocKhanh0507 <you@example.com>
2026-08-27 23:08:18 +0700

    Initial commit

---
+++ TqkLesson02Demo/Controllers/TqkProductController1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TqkLesson02Demo.Models;
+
+namespace TqkLesson02Demo.Controllers
+{
+    public class TqkProductController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            // Đưa dữ liệu ra view
+
+            ViewBag.name = "Quốc Khánh";
+            ViewData["address"] = "Fit NTU";
+            TempData["UNI"] = "Trường Đại Học Nguyễn Trãi ";
+
+            return View();
+        }
+
+        // chi tiết sản phẩm
+        public IActionResult GetProduct()
+        {
+            //Mock data
+            TqkProduct product = new TqkProduct()
+            {
+                ProductId = "P001",
+                ProductName = "Laptop Dell Vostro",
+                YearRelease = 2024,
+                Price = 12000000,
+            };
+
+            ViewData["productVD"] = product;
+            ViewBag.productVB = product;
+
+            return View();
+        }
+    }
+}
---
+++ TqkLesson02Demo/Models/TqkProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace TqkLesson02Demo.Models
+{
+    public class TqkProduct
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public  int YearRelease { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
